d6b0748dd8de2b77aff31e10e64311459ffc7b1a
zecollokaris <collo.kariss@gmail.com>
2018-09-08 16:06:32 +0300

    made changes to shellscript

--- newDjango.sh
+++ newDjango.sh
@@ -15,36 +15,17 @@ touch .gitignore
 touch README.md
 cat >> README.md << EOF
 # ${1}
-## By **Wesley Mutwiri**
-## Live Demo
-coming soon
-## Description
+## By **Collins Kariuki**
 
-## Setup and Installation Requirements
-1. Open your default web browser
-2. Internet connection is required
-3. Search for wesleymutwiri on github and click on the repository ${1}
-4. No other downloads or plug-ins are required to view this python application
-5. Click on the live site on the github repository
-6. Alternatively, use the livelink found down below
+## Description
 
 ## Known bugs
 There are currently no known bugs experienced on the website but feedback on bugs experienced during use of the website will be highly appreciated. The only reason that the website may fail to load is if the Internet connection is slow or disconnected which will require you to troubleshoot your Internet connection. Thankyou in advance for your support.
 
-## Technologies used
-* HTML5
-* CSS
-* Bootstrap4
-* Python3.6
-* django
-* Heroku
-* Git
-
 ## Support and contact details
 Contact me on my github account
-<https://github.com/wesleymutwiri>
+<https://github.com/zecollokaris>
 
-copyright (c) {2018} {Wesley Mutwiri}
 EOF
 mkdir static
 cd static
