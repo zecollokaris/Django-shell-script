73045fbd26324650c8c6d91bd06003891a339191
zecollokaris <collo.kariss@gmail.com>
2018-09-10 09:03:19 +0300

    Made changes to file newDjango.sh

--- newDjango.sh
+++ newDjango.sh
@@ -15,15 +15,31 @@ touch .gitignore
 touch README.md
 cat >> README.md << EOF
 # ${1}
-## Description
+
+# Description
+
+# Prerequisites
+
+# Setup/Installation Reqiurements.
+**{follow the below instructions for set up.}**
+
+# Technologies Used
+
+# Support and Contact
+
+-Mobile number: (+254) 798731203
+
+-Email Address: collo.kariss@gmail.com
+
+-github-username: zecollokaris
+
+## License
+
+The app is licensed by MIT.
+
+Collins Kariuki - MIT (c)2018 LICENSE
 
 
-
-## Support and contact details
-Contact me on my github account
-<https://github.com/zecollokaris>
-
-copyright (c) {2018} {Wesley Mutwiri}
 EOF
 mkdir static
 cd static
